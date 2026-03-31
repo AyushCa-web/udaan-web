3b9426521c588a48177722d26361dc940971d3bc
AyushCa-web <caaayushmaheshwari@gmail.com>
2026-03-31 14:51:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.DEPLOY_TARGET === 'ghpages';
-
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? '/udaan-web' : '',
-  assetPrefix: isGithubPages ? '/udaan-web/' : '',
   images: {
     unoptimized: true,
   },
